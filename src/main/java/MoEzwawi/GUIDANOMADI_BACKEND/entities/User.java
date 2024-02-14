572e16c4e74632f7a020f0a40faca1ce3cdf3e09
MoEzwawi <mezwawi96@gmail.com>
2024-02-14 18:02:37 +0100

    adds user entity

---
+++ src/main/java/MoEzwawi/GUIDANOMADI_BACKEND/entities/User.java
@@ -0,0 +1,63 @@
+package MoEzwawi.GUIDANOMADI_BACKEND.entities;
+
+import MoEzwawi.GUIDANOMADI_BACKEND.entities.enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@JsonIgnoreProperties({"password", "authorities", "accountNonExpired", "enabled", "accountNonLocked", "credentialsNonExpired"})
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String avatarUrl;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/MoEzwawi/GUIDANOMADI_BACKEND/entities/enums/Role.java
@@ -0,0 +1,6 @@
+package MoEzwawi.GUIDANOMADI_BACKEND.entities.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}

6156a9ab150c29ee8118901820b45ef15ffc4748
MoEzwawi <mezwawi96@gmail.com>
2024-02-14 17:47:40 +0100

    initializes spring boot project

---
+++ src/main/java/MoEzwawi/GUIDANOMADI_BACKEND/GuidanomadiBackendApplication.java
@@ -0,0 +1,13 @@
+package MoEzwawi.GUIDANOMADI_BACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidanomadiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuidanomadiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MoEzwawi/GUIDANOMADI_BACKEND/GuidanomadiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package MoEzwawi.GUIDANOMADI_BACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuidanomadiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

b3634e0d599944158632cc6eefee28725132e779
MoEzwawi <mezwawi96@gmail.com>
2024-02-28 23:28:50 +0100

    exception management hotfix

--- src/main/java/MoEzwawi/GUIDANOMADI_BACKEND/security/JWTAuthFilter.java
+++ src/main/java/MoEzwawi/GUIDANOMADI_BACKEND/security/JWTAuthFilter.java
@@ -1,19 +1,23 @@
 package MoEzwawi.GUIDANOMADI_BACKEND.security;
 
 import MoEzwawi.GUIDANOMADI_BACKEND.entities.User;
+import MoEzwawi.GUIDANOMADI_BACKEND.exceptions.NotFoundException;
 import MoEzwawi.GUIDANOMADI_BACKEND.exceptions.UnauthorizedException;
 import MoEzwawi.GUIDANOMADI_BACKEND.services.UsersService;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -24,21 +28,28 @@ public class JWTAuthFilter extends OncePerRequestFilter {
     private JWTTools jwtTools;
     @Autowired
     private UsersService usersService;
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver exceptionResolver;
 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            throw new UnauthorizedException("Bearer token required in \"Authorization\" header!");
-        } else {
-            String accessToken = authHeader.substring(7);
-            this.jwtTools.verifyToken(accessToken);
-            String id = this.jwtTools.extractIdFromToken(accessToken);
-            User user = this.usersService.findById(UUID.fromString(id));
-            Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            filterChain.doFilter(request, response);
+        try {
+            String authHeader = request.getHeader("Authorization");
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                throw new UnauthorizedException("Bearer token required in \"Authorization\" header!");
+            } else {
+                String accessToken = authHeader.substring(7);
+                this.jwtTools.verifyToken(accessToken);
+                String id = this.jwtTools.extractIdFromToken(accessToken);
+                User user = this.usersService.findById(UUID.fromString(id));
+                Authentication authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                filterChain.doFilter(request, response);
+            }
+        } catch (ExpiredJwtException | NotFoundException | UnauthorizedException ex){
+            exceptionResolver.resolveException(request, response, null, ex);
         }
     }
     @Override
